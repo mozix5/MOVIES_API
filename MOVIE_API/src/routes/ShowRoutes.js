748546508370f9094e340190c58e6abb4bfd46c1
mozix5 <76263118+mozix5@users.noreply.github.com>
2023-10-06 20:30:10 +0530

    Update ShowRoutes.js

--- MOVIE_API/src/routes/ShowRoutes.js
+++ MOVIE_API/src/routes/ShowRoutes.js
@@ -1,11 +1,16 @@
-const express=require('express')
-const auth = require('../middlewares/auth')
-const {createShow,deleteShow,getShow}=require('../controllers/showController')
-const showRouter=express.Router()
+const express = require("express");
+const auth = require("../middlewares/auth");
+const {
+  createShow,
+  deleteShow,
+  getShow,
+  isShowInCollection,
+} = require("../controllers/showController");
+const showRouter = express.Router();
 
-showRouter.get("/",auth,getShow)
-showRouter.post("/",auth,createShow)
-showRouter.delete("/:id",auth,deleteShow)
+showRouter.get("/", auth, getShow);
+showRouter.get("/:id/:showId", auth, isShowInCollection);
+showRouter.post("/", auth, createShow);
+showRouter.delete("/:id", auth, deleteShow);
 
-
-module.exports=showRouter
+module.exports = showRouter;
